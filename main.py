45439859d314b7da912815da4e96825d2bfa6e33
hayate.tanaka <tanattyokana@gmail.com>
2019-03-20 21:41:30 +0900

    add function send_message

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import time
 from slackclient import SlackClient
 from operator import itemgetter
 
 def main():
+
     slack_token = os.getenv("SLACK_TOKEN")
     client = SlackClient(slack_token)
 
@@ -11,6 +13,7 @@ def main():
     # nameに応じてソート
     channel_jsn.sort(key=itemgetter('name'))
 
+    list_txt = ''
     for jsn_key in channel_jsn:
         if jsn_key['is_archived'] == False:
 
@@ -18,9 +21,24 @@ def main():
                 topic = '[Topic: ' + jsn_key['topic']['value'] + ']'
             else:
                 topic = ''
-            print('#' + jsn_key['name'] + ' `' + jsn_key['purpose']['value'] + topic + '`')
+            txt = '<#' + jsn_key['id'] + '> `' + jsn_key['purpose']['value'] + topic + '`' + '\n'
+            list_txt += txt
+
+
+    send_message(list_txt)
+
+def send_message(txt):
+
+    slack_token_whale = os.getenv('SLACK_TOKEN_WHALE')
+    sc = SlackClient(slack_token_whale)
+
+    if sc.rtm_connect():
+        sc.rtm_send_message("test", txt)
+    else:
+        print("Connection Failed, invalid token?")
 
 def channel_list(client):
+
     channels = client.api_call("channels.list")
     if channels['ok']:
         return channels['channels']
